dc4e4336a0f1cddb8e17eea2e6af069ff33caf22
Tanzim Siddiqee <tanzimsiddiqee@hotmail.com>
2021-07-10 14:11:17 +0600

    file upload code added

--- soilproject/views.py
+++ soilproject/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import redirect, render
 from .models import Students
+from .forms import StudentForm
+from django.core.files.storage import FileSystemStorage
 
 # Create your views here.
 
@@ -7,8 +9,25 @@ def index(request):
     context = { 'students' : Students.objects.all() }
     return render(request,'index.html',context)
 
-def about(request):
-   return render(request,'about.html')
+def student(request):
+     if request.method == 'POST':
+        form = StudentForm(request.POST, request.FILES)
+        if form.is_valid():    
+            fs = FileSystemStorage()
+            uploadedfile = request.FILES['Image']
+            file = fs.save(uploadedfile.name, uploadedfile)
+
+            std = Students()
+            std.ID = Students.objects.last().ID + 1
+            std.Name = form.cleaned_data['Name']
+            std.ImagePath = fs.url(file)
+            std.save()
+            
+            return redirect("/")
+
+     else:
+        form = StudentForm()
+        return render(request,'form.html', {'form': form})
 
 def contact(request):
      return render(request,'contact.html')
